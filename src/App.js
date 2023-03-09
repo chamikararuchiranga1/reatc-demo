c7c48eb7d6eb639f3146c7eb00adc26deeaa7e3d
chami <chamikararuchiranga1@gmail.com>
2023-03-09 21:12:06 +0530

    use state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import './App.css';
-import {Routes, Route, Navigate} from "react-router-dom";
-import Home from "./pages/Home/Home";
+import {Navigate, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home/Home';
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Button from "./common/components/Button/Button";
+import {useState} from "react";
 
 function App() {
+
+    const [plus, setPlus] = useState(0);
+
   return (
     <div className='App'>
-        <Routes>
-            <Route path="*" element={<Navigate to="/dashboard"/>}/>
-            <Route path={"/home"} element={<Home/>} key={'home'}/>
-            <Route path={"/dashboard"} element={<Dashboard/>} key={'home'}/>
-        </Routes>
+        <Button name={'CLick +'} onClick={()=> setPlus(plus+1)}/>
+        <Button name={'CLick -'} onClick={()=> setPlus(plus-1)}/>
+
+        <div>
+            <h1>Value as : {plus}</h1>
+        </div>
+
+
+      <Routes>
+          {/*<Route path='*' element={<Navigate to={'/dashboard'}/>} key={'home'}/>*/}
+          <Route path={'/home'} element={<Home/>} key={'home'}/>
+          <Route path={'/dashboard'} element={<Dashboard/>} key={'home'}/>
+      </Routes>
     </div>
   );
 }
